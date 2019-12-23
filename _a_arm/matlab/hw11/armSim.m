64c90147115425cfeadea469e7b5f7782eb6d9d4
Tarnarmour <Tarnarmour@gmail.com>
2019-12-23 14:34:36 -0700

    Removed disturbance and added alpha = 0.0

--- _a_arm/matlab/hw11/armSim.m
+++ _a_arm/matlab/hw11/armSim.m
@@ -1,10 +1,11 @@
 armParamHW11;  % load parameters
 
 % instantiate arm, controller, and reference input classes 
-addpath('../hw3'); arm = armDynamics(P);  
+alpha = 0.0;
+addpath('../hw3'); arm = armDynamics(alpha,P);  
 controller = armController(P);  
 addpath('../hw2'); reference = signalGenerator(30*pi/180, 0.05);  
-addpath('../hw2'); disturbance = signalGenerator(0.25, 0.0);
+addpath('../hw2'); disturbance = signalGenerator(0, 0.0);
 
 % instantiate the data plots and animation
 addpath('../hw2'); dataPlot = dataPlotter(P);
@@ -20,7 +21,7 @@ while t < P.t_end
         r = reference.square(t);
         d = disturbance.step(t);
         n = 0;  % noise
-        x = arm.state
+        x = arm.state;
         u = controller.update(r, x);  % Calculate the control value
         y = arm.update(u+d);  % Propagate the dynamics
         t = t + P.Ts; % advance time by Ts
