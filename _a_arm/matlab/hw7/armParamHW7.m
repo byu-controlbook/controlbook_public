655390d2cea75f9dabce25d27d492aea6fd1bffb
Tarnarmour <Tarnarmour@gmail.com>
2019-12-23 12:57:13 -0700

    Removed section that pertained to future homework

--- _a_arm/matlab/hw7/armParamHW7.m
+++ _a_arm/matlab/hw7/armParamHW7.m
@@ -2,11 +2,6 @@
 addpath ./.. % adds the parent directory to the path
 armParam % general arm parameters
 
-%  tuning parameters
-%tr = 0.8; % part (a)
-tr = 0.4;  % tuned to get fastest possible rise time before saturation.
-zeta = 0.707;
-
 % equalibrium angle and torque
 P.theta_e = 0*pi/180;
 P.tau_e   = P.m*P.g*P.ell/2*cos(P.theta_e);
