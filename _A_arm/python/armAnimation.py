a77fe218ab0abeee75a14e59112ea46c1d7a9a4e
Randy Beard <beard@byu.edu>
2023-01-16 21:06:14 -0700

    Update armAnimation.py

--- _A_arm/python/armAnimation.py
+++ _A_arm/python/armAnimation.py
@@ -1,5 +1,3 @@
-import sys
-sys.path.append('..')  # add parent directory
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import numpy as np
@@ -7,31 +5,21 @@ import armParam as P
 
 
 class armAnimation:
-    '''
-        Create arm animation
-    '''
     def __init__(self):
         # Used to indicate initialization
         self.flagInit = True
-
         # Initializes a figure and axes object
         self.fig, self.ax = plt.subplots()
-
         # Initializes a list object that will be used to
         # contain handles to the patches and line objects.
         self.handle = []
-
         self.length=P.length
         self.width=P.width
-
         # Change the x,y axis limits
         plt.axis([-2.0*P.length, 2.0*P.length, -2.0*P.length,
                   2.0*P.length])
-
         # Draw a base line
         plt.plot([0, P.length], [0, 0],'k--')
-
-
         # Draw pendulum is the main function that will call the
         # functions:
         # drawCart, drawCircle, and drawRod to create the animation.
@@ -39,10 +27,8 @@ class armAnimation:
     def update(self, u):
         # Process inputs to function
         theta = u[0][0]   # angle of arm, rads
-
         X = [0, self.length*np.cos(theta)]  # X data points
         Y = [0, self.length*np.sin(theta)]  # Y data points
-
         # When the class is initialized, a line object will be
         # created and added to the axes. After initialization, the
         # line object will only be updated.
