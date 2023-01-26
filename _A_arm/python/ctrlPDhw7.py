fbcd923f33b7277cfd64437126c066914687ac7d
Randy Beard <beard@byu.edu>
2023-01-25 21:10:14 -0700

    Update ctrlPDhw7.py

--- _A_arm/python/ctrlPDhw7.py
+++ _A_arm/python/ctrlPDhw7.py
@@ -3,7 +3,6 @@ import armParam as P
 
 class ctrlPD:
     def __init__(self):
-        tau_max = P.tau_max  # limit on control signal
         # PD gains
         self.kp = 0.18
         self.kd = 0.095
@@ -13,17 +12,13 @@ class ctrlPD:
     def update(self, theta_r, x):
         theta = x[0][0]
         thetadot = x[1][0]
-
         # feedback linearized torque
         tau_fl = P.m * P.g * (P.ell / 2.0) * np.cos(theta)
-
         # equilibrium torque around theta_e = 0
         theta_e = 0.0
         tau_e = P.m * P.g * P.ell / 2.0 * np.cos(theta_e)
-
         # compute the linearized torque using PD control
         tau_tilde = self.kp * (theta_r - theta) - self.kd * thetadot
-
         # compute total torque
         tau = tau_fl + tau_tilde
         #tau = tau_e + tau_tilde
