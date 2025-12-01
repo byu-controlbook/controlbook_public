c3f5d635292b20a0781cf6f1e596bfa2dc1f0a15
Mat Haskell <mhaskell9@gmail.com>
2025-12-01 16:20:44 -0700

    Updated Arm bode plots for compatibility with newer control module

--- _A_arm/python/hw17.py
+++ _A_arm/python/hw17.py
@@ -1,53 +1,48 @@
 # Single link arm Parameter File
 import hw16 as P16
-from control import bode, margin, mag2db
+import control as cnt
 import matplotlib.pyplot as plt
 
 # flag to define if using dB or absolute scale for M(omega)
-dB_flag = P16.dB_flag
+dB_flag = False
 
 # assigning plant and controller from past HW (to make sure
 # we don't introduce additional errors)
 Plant = P16.Plant
 C_pid = P16.C_pid
 
-if __name__ == '__main__':
-    # Calculate the phase and gain margins
-    if dB_flag:
-        gm, pm, Wcg, Wcp = margin(Plant*C_pid)
-        gm = mag2db(gm)
-        print("Inner Loop:", "gm: ",gm,
-              " pm: ", pm," Wcg: ", Wcg, " Wcp: ", Wcp)
+sys_open_loop = C_pid * Plant
+sys_closed_loop = sys_open_loop / (1 + sys_open_loop)
 
-    else:
-        gm, pm, Wcg, Wcp = margin(Plant*C_pid)
-        print("Inner Loop:", "gm: ", gm,
-              " pm: ", pm, " Wcg: ", Wcg, " Wcp: ", Wcp)
+if __name__ == "__main__":
+    # Calculate the phase and gain margins
+    gm, pm, Wcg, Wcp = cnt.margin(sys_open_loop)
+    if dB_flag:
+        gm = cnt.mag2db(gm)
+    print("Inner Loop:")
+    print(f"\tgain margin: {gm:.2f}, at Wc = {Wcg:.2f}")
+    print(f"\tphase margin: {pm:.2f}, at Wc = {Wcp:.2f}")
 
     # display bode plots of transfer functions
-    fig1 = plt.figure()
+    fig = plt.figure()
+    axes = fig.subplots(2, sharex=True)
 
     # this makes two bode plots for open and closed loop
-    bode([Plant * C_pid, 
-          Plant*C_pid/(1+Plant*C_pid)], 
-          dB=dB_flag)
-    
+    cnt.bode(
+        [sys_open_loop, sys_closed_loop],
+        dB=dB_flag,
+        ax=axes,
+        label=["Open-loop", "Closed-loop"],
+    )
+
     # now we can add lines to show where we calculated the GM and PM
-    fig1.axes[0].plot([Wcg, Wcg],
-                      plt.ylim(), 'k--', label='GM')
-    fig1.axes[1].plot([Wcp, Wcp], plt.ylim(), 'b--', label='PM')
+    axes[0].plot([Wcg, Wcg], axes[0].get_ylim(), "k--", label="Gain Margin")
+    axes[1].plot([Wcp, Wcp], axes[1].get_ylim(), "b--", label="Phase Margin")
 
-    # finally, we can format and add legends for clarity
-    mag_legend = ['C(s)P(s) - Open-loop', 
-                  r'$\frac{P(s)C(s)}{1+P(s)C(s)}$ - Closed-loop', 
-                  'GM']
-    phase_legend = ['Open-loop', 'Closed-loop', 'PM']
-    fig1.axes[0].legend(mag_legend)
-    fig1.axes[1].legend(phase_legend)
+    axes[0].legend()
+    axes[1].legend()
 
-    # setting axis title
-    fig1.axes[0].set_title('Single Link Robot Arm - '+
-                           'GM:'+str(round(gm, 2))+
-                           ', PM:'+str(round(pm, 2)))
-    fig1.suptitle('Bode Plots for Single Link Robot Arm')
+    fig.suptitle("Bode Plots: Single Link Arm")
+    axes[0].set_title(f"GM: {gm:.2f}, PM: {pm:.2f}")
+
     plt.show()
